fb04a404ab4b251cecff9082c48ae6b3167fafa3
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-03-06 14:04:24 -0800

    adding places controller

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use('places', require("./controllers/places"))
+
 app.get("/", (req,res){
     res.send("hello world")
 });
 
 app.get("*", (req,res){
     res.status(404).send(console.log(`<h1>404 Page not Found</h1>`))
-})
+});
+
 app.listen(process.env.PORT,()=>{
     console.log("listening on port 3002")
 });
