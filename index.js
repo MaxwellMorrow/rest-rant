4529a4525ff3a918684386c16e50ec1af3732596
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-03-06 14:10:43 -0800

    adding remaining stub routes

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.use('places', require("./controllers/places"))
+app.use('/places', require("./controllers/places"))
 
-app.get("/", (req,res){
+app.get("/", (req,res)=>{
     res.send("hello world")
 });
 
-app.get("*", (req,res){
-    res.status(404).send(console.log(`<h1>404 Page not Found</h1>`))
+app.get("*", (req,res)=>{
+    res.status(404).send(`<h1>404 Page not Found</h1>`)
 });
 
 app.listen(process.env.PORT,()=>{
