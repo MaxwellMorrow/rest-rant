691b5ddec433868fe5c2d216e26d04b3a3a9f3f4
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-03-15 14:20:43 -0700

    delete route implimented but buggy

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-// environment  required
+// Modules and Globals
 require("dotenv").config();
-
 const express = require("express");
+const methodOverride = require("method-override");
 const app = express();
 
 // middleware
-app.set("views",__dirname + "/views")
-app.set("view engine","jsx")
-app.engine("jsx",require("express-react-views").createEngine())
-app.use(express.static("public"))
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(methodOverride("_method"))
 
 app.use(express.urlencoded({ extended: true }));
 // controllers
 app.use("/places", require("./controllers/places"));
 
-app.get("/", (req,res)=>{
-    res.render("home")
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get("*", (req,res)=>{
-    res.render("error404")
+app.get("*", (req, res) => {
+  res.render("error404");
 });
 
-app.listen(process.env.PORT,()=>{
-    console.log("listening on port 3002")
+app.listen(process.env.PORT, () => {
+  console.log("listening on port 3002");
 });
