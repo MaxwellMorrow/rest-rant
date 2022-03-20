e919e513c66de9d8e81a2f00d5d546c0a0adec2c
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-03-20 13:37:54 -0700

    added seeding data and validations

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,7 +5,10 @@ function show(data) {
   return (
     <Def>
       <main>
-        <h1>{data.place.name}</h1>\
+        <h1>{data.place.name}</h1>
+        <h3> Located in {data.place.city}, {data.place.state}</h3>
+        <h3>{data.place.showEstablished()}</h3>
+        <h4>Serving {data.place.cuisines}</h4>
         <a href={`/places/${data.id}/edit`} className="btn btn-warning">
           Edit
         </a>
